7aac7dbbd17a0ae90c0f35ac012695e8452c0ee5
ayushjava07 <ayushjhasahab07@gmail.com>
2026-04-27 21:41:26 +0530

    Refactor: update main method to initialize FileController and handle application lifecycle

--- p2p/src/main/java/p2p/App.java
+++ p2p/src/main/java/p2p/App.java
@@ -1,10 +1,25 @@
 package p2p;
 
+import p2p.controllers.FileController;
+
 /**
  * Hello world!
  */
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        try{
+            FileController fileController = new FileController(8080);
+            fileController.start();
+            System.out.println("PeerLink is running on port 8080");
+            System.out.println("UI is available at http://localhost:8080");
+            Runtime.getRuntime().addShutdownHook(new Thread(() -> {
+                System.out.println("Shutting down PeerLink...");
+                fileController.stop();
+            }));
+            System.out.println("Press ENTER to stop the application.");
+            System.in.read();
+        } catch (Exception e) {
+            e.printStackTrace();    
+        }
     }
 }
