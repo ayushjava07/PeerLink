76e5db1964aa4af37cb71845dea7f4fa72da47a8
ayushjava07 <ayushjhasahab07@gmail.com>
2026-04-27 22:10:00 +0530

    chore: configure static export and migrate proxy rewrites to Netlify redirects

--- ui/next.config.js
+++ ui/next.config.js
@@ -1,19 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: "export",
   reactStrictMode: true,
   swcMinify: true,
-  async rewrites() {
-    return [
-      {
-        source: '/api/upload',
-        destination: 'http://localhost:8080/upload',
-      },
-      {
-        source: '/api/download/:port',
-        destination: 'http://localhost:8080/download/:port',
-      },
-    ];
-  },
-}
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
